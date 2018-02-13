ed6c42ab4d340df85d72e6a2e1ec573e3ee9edd7
Wampa842 <wampa842@gmail.com>
2018-02-13 02:57:03 +0100

    implemented remote time update

--- warframe-tracker/js/clock.js
+++ warframe-tracker/js/clock.js
@@ -1,3 +1,58 @@
+
+/* Fetches the Unix timestamp of the last time the Cetus bounties reset, from the official world state file(s).
+ * The first parameter defines the platform: 1 is PC, 2 is PS4 and 3 is XBox 1.
+ * 0, null or undefined doesn't fetch the data and returns a static timestamp.
+ * The Cetus time is then passed to the callback function. I know it's a mess - I'll try to clean it up when I have more time and coffee.
+ */
+function getCetusTime(platform, callback)
+{
+	var timestamp = 1510884902;	//Static timestamp to be returned in case of an error. Correct as of 2018-02-13, for PC version 22.12.2. Might not be accurate in the future.
+	if(!platform || (platform > 3))
+	{
+		callback(timestamp);
+		return;
+	}
+
+	var worldStateUrls =
+	[
+		"http://content.warframe.com/dynamic/worldState.php",
+		"http://content.ps4.warframe.com/dynamic/worldState.php",
+		"http://content.xb1.warframe.com/dynamic/worldState.php"
+	];
+
+	var worldStateUrl = "http://www.whateverorigin.org/get?url=" + encodeURIComponent(worldStateUrls[platform-1]) + "&callback=?";
+
+	$.ajax(
+	{
+		url: worldStateUrl,
+		dataType: "json",
+		mimeType: "application/json",
+		success: function(data)
+		{
+			var worldStateData
+			try
+			{
+				worldStateData = JSON.parse(data.contents); //whateverorigin is a little weird in that it returns the requested data as a string in a JSON response
+			}
+			catch(e)
+			{
+				console.warn("Could not fetch Cetus time (", e.message, "). Using static timestamp. Accuracy not guaranteed.");
+				callback(timestamp);
+				return;
+			}
+			var syndicate = worldStateData["SyndicateMissions"].find(element => (element["Tag"] == "CetusSyndicate"));
+			timestamp = Math.floor(syndicate["Activation"]["$date"]["$numberLong"] / 1000);	//The activation time, converted to whole seconds
+			console.log("Fetched Cetus time: ", timestamp);
+			callback(timestamp);
+		},
+		failure: function(xhr, status, error)
+		{
+			console.warn("Cound not fetch Cetus time:", status, error, ". Using static timestamp. Accuracy not guaranteed.");
+			callback(timestamp);
+		}
+	});
+}
+
 function analogClockUpdate(min)
 {
 	var angle_h = (min % 720) / 720 * 360;
@@ -10,19 +65,20 @@ function analogClockUpdate(min)
 	hand_m.css("transform", "rotate(" + angle_m + "deg)");
 }
 
-function cetusClockUpdate()
+function minToString(min)
 {
-	function minToString(min)
-	{
-		var h = Math.floor((min / 60) % 24);
-		var m = Math.floor(min % 60);
-		var s = Math.floor((min * 60) % 60);
-		return h + ":" + m.toString().padStart(2, "0") + ":" + s.toString().padStart(2, "0");
-	};
+	var h = Math.floor((min / 60) % 24);
+	var m = Math.floor(min % 60);
+	var s = Math.floor((min * 60) % 60);
+	return h + ":" + m.toString().padStart(2, "0") + ":" + s.toString().padStart(2, "0");
+};
+
+function cetusClockUpdate(startTime)
+{
 
 	var date = new Date();
 	var now = date.getTime() / 1000;	//current time since Unix epoch in seconds
-    var startTime = 1510884902;	//time of a PoE sunrise since Unix epoch in seconds, shamelessly copied from the code of clockofeidolon.com by Lyneca (MIT license). I don't know how the fuck you came up with such a specific number, but I applaud the accuracy.
+    //var startTime = cetusTime;	//time of a PoE sunrise since Unix epoch in seconds, shamelessly copied from the code of clockofeidolon.com by Lyneca (MIT license). I don't know how the fuck you came up with such a specific number, but I applaud the accuracy.
 	var cetusTime = ((now - startTime) / 60) % 150;	//Cetus time of day in minutes
 	$(".cetus-period").html(cetusTime > 100 ? "until sunrise" : "until sunset");
 	var nextPeriod = cetusTime > 100 ? 150 : 100;
@@ -36,5 +92,14 @@ function cetusClockUpdate()
 
 $(document).ready(function(event)
 {
-	var interval = setInterval(cetusClockUpdate, 10);
+	var interval;
+	getCetusTime(0, function(time)
+	{
+		interval = setInterval(cetusClockUpdate, 10, time);
+	});
+	getCetusTime(1, function(time)
+	{
+		clearInterval(interval);
+		interval = setInterval(cetusClockUpdate, 10, time);
+	});
 });
